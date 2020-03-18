0e5035a3b393fc42daacd2fb4cf81e83207c9b60
Neibye <60384297+Neibye@users.noreply.github.com>
2020-03-18 11:37:03 +0100

    Version 37

--- js/script.js
+++ js/script.js
@@ -4,46 +4,15 @@ thebutton.addEventListener("click", function(){
     window.location.href = "book.html";
 });
 
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
 
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
-})
-
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
-})
-
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
-})
-
-function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
-}
-
-function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
+if(document.getElementById("educationinput").selectedIndex == multimediedesigner) {
+    alert("test");
 }
 
 
+/*
 document.getElementById("educationinput").onchange = function() {
     
-    document.getElementById("thebutton").href = 'http://book.html/"+this.value+"/';
+    thebutton.href = 'book.html/"+this.value+"/';
     
-}
+}*/
