7c1519714ed32eedf51344af269ac3a9b0640ee0
woodbully <60384319+woodbully@users.noreply.github.com>
2020-03-20 09:02:07 +0100

    Version 63
    Drop down virker nu.....!!!!!!

--- js/script.js
+++ js/script.js
@@ -65,22 +65,6 @@ function changeBackgroundColorSeven() {
 
 
 
-let classByCategory = {
-    A: ["E19-a", "E19-b", "E19-c", "E19-e", "E19-x"],
-    B: ["No options"],
-}
-
-    function changecat(value) {
-        if (value.length == 0) document.getElementById("category").innerHTML = "<option></option>";
-        else {
-            let catOptions = "";
-            for (categoryId in classByCategory[value]) {
-                catOptions += "<option>" + classByCategory[value][categoryId] + "</option>";
-            }
-            document.getElementById("category").innerHTML = catOptions;
-        }
-    };
-
 
 const backToTopBtn = document.querySelector("#back-to-top-btn");
 const downArrow = document.querySelector("#down-arrow");
